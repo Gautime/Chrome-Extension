f81301c18742325aaf92143fef739e3cfe73d869
Kapil <gautime.idk@gmail.com>
2019-03-18 18:09:00 +0530

    Removed inline js functions

--- contentScript.js
+++ contentScript.js
@@ -1,36 +1,22 @@
 console.log("Chrome extension loaded");
-	
 	var all=document.body.getElementsByTagName('*');
+
 	var style = document.createElement('style');
 	style.type = 'text/css';
 	style.innerHTML = '.h:hover { background-color: #83aff7; }';
 	document.getElementsByTagName('head')[0].appendChild(style);
-
+	var all=document.body.getElementsByTagName('*');
+	
 	for (var i = 0, max=all.length;i<max; i++)
 
 	 {
-		all[i].setAttribute('onclick','getid(this)');
-		all[i].setAttribute('onmouseover','geth(this)');
+		all[i].addEventListener("click",function(){
+		console.log(this);
+		console.log(window.getComputedStyle(this));});
+		all[i].addEventListener("mouseover",function(){
+		this.setAttribute('class','h');});
+		/*all[i].setAttribute('onclick','getid(this)');
+		all[i].setAttribute('onmouseover','geth(this)');*/
 
 	}
 	
-	/*for (var i = 0, max=all.length;i<max; i++) 
-	{
-		var el=all[i];
-		el.addEventListener('mouseover',function(){el.style['background-color']='#83aff7'});
-	}*/
-
-	function geth(el)
-	{
-	
-		el.setAttribute('class','h');
-
-	}
-	function getid(el)
-	{
-		console.log(el);
-		console.log(window.getComputedStyle(el));
-
-	}
-	
-
