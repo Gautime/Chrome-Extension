0ba1144607f6344af97e7176182e6ecfa13cc1b6
Kapil <gautime.idk@gmail.com>
2019-03-26 21:07:13 +0530

    Updated output process

--- contentScript.js
+++ contentScript.js
@@ -2,7 +2,6 @@ console.log("Chrome extension loaded");
 
 
 
-	console.log('Jquery Loaded');
   
 var all=document.body.getElementsByTagName('*');
 var divs=document.body.getElementsByTagName('*');
@@ -12,33 +11,39 @@ var divs=document.body.getElementsByTagName('*');
 
 	var style = document.createElement('style');
 	style.type = 'text/css';
-	style.innerHTML = '.h:hover { background-color: #83aff7; }';
+	style.innerHTML = '.h *:hover { background-color: #83aff7; }';
 	document.getElementsByTagName('head')[0].appendChild(style);
 
 	//var divs = document.querySelectorAll("div:not(.first)").children;
-
+	console.clear();
 	for (var i = 0, max=all.length;i<max; i++)
 
 	 {
 		all[i].addEventListener("click",function(){
 		console.log(this);
+
+		var el=JSON.stringify(this);
+		
+		document.write("Check the console");
 		console.log(window.getComputedStyle(this));});
 	}
 	for (var i = 0, ma=divs.length; i <ma; i++)
 	{
 	
+		var el=this.parentNode;
+		while(el)
+		{
+		divs[i].addEventListener('mouseover', function(){
+  		el.classList.remove('h');
+		});
+		el=this.parentNode;	
+		}
+
 		(divs[i]).addEventListener('mouseover', function(){
   		this.classList.add('h');
 		});
 
-		var el;
-		while(el)
-		{
-		divs[i].addEventListener('mouseover', function(){
-  		this.parentNode.classList.remove('h');
-		});
-		el=this.parentNode;	
-		}
+		
 
 
 		/*divs[i].addEventListener("mouseenter",function(){
